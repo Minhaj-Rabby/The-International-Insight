f986b0d33016e002a8c8753dea6fe80082c617a3
Minhaj-Rabby <minhaj12113@gmail.com>
2023-10-06 18:46:58 +0600

    Env File Updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAS88buF6w_EnxBavOT5ZtaP1gzSjxQ0uY",
-  authDomain: "the-international-insight.firebaseapp.com",
-  projectId: "the-international-insight",
-  storageBucket: "the-international-insight.appspot.com",
-  messagingSenderId: "1053250685702",
-  appId: "1:1053250685702:web:63a3e846660d7241936c87"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
